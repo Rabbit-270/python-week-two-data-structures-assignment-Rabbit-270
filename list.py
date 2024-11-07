e86b36cd051ba2f1bad4417f35ea27a35f54ebc2
root <root@A>
2024-11-07 13:41:57 +0200

    I have completed week two's python assignment

---
+++ list.py
@@ -0,0 +1,26 @@
+'''
+Assignment for Week 2, data structures.
+'''
+# question 1 
+my_list = []
+
+# question 2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# question 3
+my_list[1] = 15
+
+# question 4
+my_list.extend([50, 60, 70])
+
+# question 5
+del my_list[-1]
+
+# question 6
+my_list.sort()
+
+# question 7
+print(my_list.index(30))
